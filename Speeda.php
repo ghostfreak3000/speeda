f79c9372146025553cab0eca7bb5460dd42fd32d
Bishaka Samuel <sbishaka@gmail.com>
2016-01-23 14:04:14 +0300

    Add basic routing

---
+++ Speeda.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bishaka
+ * Date: 23/01/2016
+ * Time: 13:57
+ */
+
+class Speeda {
+
+    function helloWorld(){
+        $result[0] = array("msg"=>"Hello, World");
+        return $result;
+    }
+
+}
